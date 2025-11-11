538f3f94f2ed31cb567f9ed02ea87375cef490b0
Severin Leander <gasserseverin1@gmail.com>
2025-11-11 04:40:05 +0100

    Update cache version in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rp-v3_2';
+const CACHE='rp-v3_1';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/carnegie-exit.pdf','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))));
